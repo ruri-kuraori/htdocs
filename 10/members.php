a03fa549b5d53528ff66e4cb59e2fd9a70dc298a
ruri.kuraori <ruri.kuraori@gmail.com>
2024-01-16 20:51:31 +0900

    first-commit

---
+++ 10/members.php
@@ -0,0 +1,91 @@
+<?php
+
+$members = [
+  [
+    'id' => 11,
+    'family_name' => '山田',
+    'first_name' => '太郎',
+    'age' => 35,
+    'gender' => '男性',
+    'address' => '東京都港区',
+  ],
+  [
+    'id' => 12,
+    'family_name' => '佐藤',
+    'first_name' => '花子',
+    'age' => 28,
+    'gender' => '女性',
+    'address' => '東京都中央区',
+  ],
+  [
+    'id' => 13,
+    'family_name' => '斎藤',
+    'first_name' => '次郎',
+    'age' => 41,
+    'gender' => '男性',
+    'address' => '東京都豊島区',
+  ],
+  [
+    'id' => 14,
+    'family_name' => '山本',
+    'first_name' => '三郎',
+    'age' => 52,
+    'gender' => '男性',
+    'address' => '東京都世田谷区',
+  ],
+];
+
+
+
+?>
+<!DOCTYPE hrml>
+<html lang="ja">
+    <head>
+        <title>2次元配列</title>
+        <style>
+          table { 
+            border-collapse: collapse;
+           }
+          th,td {
+            border: solid 1px black;
+            padding: 3px;
+           }
+        </style>
+    </head>
+    <body>
+        <h1>2次元配列</h1>
+        <ul>
+  
+            <?php 
+                foreach($members as $member){
+                echo '<li>' . $member['family_name'].' '. $member['first_name'].': '.
+                $member['address']. ' '. $member['age']. '才</li>';
+                }
+            ?>
+        </ul>
+        <table>
+          <tr>
+            <th>id</th>
+            <th>姓</th>
+            <th>名</th>
+            <th>年齢</th>
+            <th>性別</th>
+            <th>住所</th>
+          </tr>
+          <?php 
+              foreach($members as $member){
+                echo '<tr>
+                <td>' . $member['id'].'</td>
+                <td>'  . $member['family_name'] .'</td>
+                <td>'  . $member['first_name'] .'</td>
+                <td>'  . $member['age'] .'</td>
+                <td>'  . $member['gender'] .'</td>
+                <td>'  . $member['address'] .'</td>
+                </tr>';
+              }?>
+          
+        </table>
+        
+        
+    </body>
+</html>
